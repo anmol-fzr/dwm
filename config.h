2adf56dd8e11bccede4dcf8ac0b7e1e4b346b178
Anmol <ainsa2279@gmail.com>
2024-01-16 01:00:28 +0530

    Before MoveResize Patch

--- config.h
+++ config.h
@@ -1,12 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 #include <X11/XF86keysym.h>
-
 /* appearance */
-static const unsigned int borderpx = 4; /* border pixel of windows */
-static const unsigned int borderalpha = 1; /* border pixel of windows */
-static const unsigned int baralpha = 8; /* border pixel of windows */
-
-static const unsigned int snap = 32;    /* snap pixel */
+static const unsigned int borderpx = 2; /* border pixel of windows */
+static const unsigned int borderalpha = 9; /* border pixel of windows */
+static const unsigned int baralpha = 25; /* border pixel of windows */
+static const unsigned int gappx = 10;
+static const unsigned int snap = 12;    /* snap pixel */
 static const int showbar = 0;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
 static const char *fonts[] = {"JetBrains Mono:size=16"};
@@ -16,7 +15,7 @@ static const char border_norm[] = "#6C7086";
 static const char color_norm[] = "#CDD6F4";
 static const char color_sel[] = "#1E1E2E";
 static const char bg_sel[] = "#B4BEFE";
-static const char border_sel[] = "#A6E3A1";
+static const char border_sel[] = "#cba6f7";
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {color_norm, bg_norm, border_norm},
@@ -67,22 +66,24 @@ static const Layout layouts[] = {
   }
 
 static const char browser[] = "chromium";
+static const char alt_browser[] = "qutebrowser";
 static const char editor[6] = "codium";
 static const char fileManager[] = "Thunar";
 /* commands */
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
-    "dmenu_run", "-m",       dmenumon, "-fn",  dmenufont, "-nb",     bg_norm,
+    "dmenu_run","-b", "-m",dmenumon, "-fn",  dmenufont, "-nb",     bg_norm,
     "-nf",       color_norm, "-sb",    bg_sel, "-sf",     color_sel, NULL};
 static const char *termcmd[] = {"kitty", NULL};
 static const char *editorcmd[] = {editor, NULL};
+static const char *emailcmd[] = { "openmails", NULL};
 static const char *browsercmd[] = {browser, NULL};
+static const char *altbrowsercmd[] = {alt_browser, NULL};
 static const char *fileManagercmd[] = {fileManager, NULL};
 static const char *musiccmd[] = {"/usr/bin/mocp", NULL};
 static const char *notescmd[] = {
     "/usr/bin/sh", "~/Apps/applications/logSeq/Logseq.AppImage", NULL};
-static const char *nvimcmd[] = {"/usr/bin/nvim", NULL};
 static const char *incVol[] = {"/usr/local/bin/incVol", NULL};
 static const char *decVol[] = {"/usr/local/bin/decVol", NULL};
 static const char *muteVol[] = {"/usr/local/bin/muteVol", NULL};
@@ -95,12 +96,14 @@ static const char *powerCmd[] = {"/usr/local/bin/powerpick", NULL};
 static const Key keys[] = {
     /* modifier                     key        function        argument */
     {MODKEY, XK_space, spawn, {.v = dmenucmd}},
+    
     {MODKEY, XK_Return, spawn, {.v = termcmd}},
     {ALT_L | ShiftMask, XK_w, spawn, {.v = browsercmd}},
+    {ALT_L | ShiftMask, XK_e, spawn, {.v = emailcmd}},
+    {ALT_L | ShiftMask, XK_q, spawn, {.v = altbrowsercmd}},
     {ALT_L | ShiftMask, XK_c, spawn, {.v = editorcmd}},
     {ALT_L | ShiftMask, XK_m, spawn, {.v = musiccmd}},
     {ALT_L | ShiftMask, XK_n, spawn, {.v = notescmd}},
-    {ALT_L | ShiftMask, XK_e, spawn, {.v = nvimcmd}},
 
     // XF86PowerDown
     {0, XF86XK_PowerDown, spawn, {.v = powerCmd}},
@@ -154,5 +157,4 @@ static const Button buttons[] = {
     {ClkTagBar, 0, Button1, view, {0}},
     {ClkTagBar, 0, Button3, toggleview, {0}},
     {ClkTagBar, MODKEY, Button1, tag, {0}},
-    {ClkTagBar, MODKEY, Button3, toggletag, {0}},
-};
+    {ClkTagBar, MODKEY, Button3, toggletag, {0}}, };
